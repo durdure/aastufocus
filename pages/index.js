3155c92a5bf193776c94ec679d8c00883348fd9d
Duresa Eshetu <duresaeshetu2001@gmail.com>
2024-10-08 00:22:28 +0300

    focus

--- pages/index.js
+++ pages/index.js
@@ -120,17 +120,7 @@ const Home = () => {
           style={{ clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)' }}></div>
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.
-               <Link href="#" className="font-semibold text-indigo-600">
-               <span className="absolute inset-0" aria-hidden="true">
-                </span>Read more <span aria-hidden="true">
-                  &rarr;
-                  </span>
-                </Link>
-            </div>
-          </div>
+         
     <div className="text-center">
            
       <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
